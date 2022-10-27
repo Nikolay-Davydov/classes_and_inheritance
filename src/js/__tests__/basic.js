b60a7f27da1b8359e0e1285148489dba96875eb5
Nikolay-Davydov <davidovns70@gmail.com>
2022-10-27 13:20:47 +0200

    inint matchers

---
+++ src/js/__tests__/basic.js
@@ -0,0 +1,16 @@
+import { healtySort } from '../matchers';
+
+test('should sort', () => {
+  const passedArray = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = healtySort(passedArray);
+  const expectedArray = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(result).toEqual(expectedArray);
+});
